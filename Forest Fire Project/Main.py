44fc71ceec6484b176a63ff46a2a1ccfacf141c6
tkbarb10 <tkirk@sandiego.edu>
2024-12-12 17:19:10 -0800

    Update for 12/12

--- Forest Fire Project/Main.py
+++ Forest Fire Project/Main.py
@@ -1,6 +1,34 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("C:/Users/Taylor/OneDrive/Desktop/VS Code Projects/Forest Fire Project/forestfires.csv")
+df = pd.read_csv('C:/Users/Taylor/OneDrive/Desktop/GitHub local repo/Forest-Fire/Forest Fire Project/forestfires.csv')
 
 df.head()
+
+"""
+Rain column is mostly 0 so dropping that
+"""
+
+df.drop(columns = 'rain', inplace = True)
+
+# Histogram of temperatures
+
+df['temp'].plot(kind = 'hist', edgecolor = 'black', bins = 20)
+plt.show()
+
+# Histogram of wind
+
+df['wind'].plot(kind = 'hist', edgecolor = 'black', bins = 20)
+plt.show()
+
+#Bar graphs of month and days
+
+months = df['month'].value_counts()
+months.plot(kind = 'bar')
+plt.show()
+
+days = df['day'].value_counts()
+days.plot(kind = 'bar')
+plt.show()
+
